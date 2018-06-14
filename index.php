9a68298bd8b1acabea4f4bbadc37458658b33cd0
unknown <paiboon@setscope.com>
2018-06-14 17:19:40 +0700

    edit jquery script for delete user export list v1.1 (index.php)

--- index.php
+++ index.php
@@ -6,15 +6,31 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="assets/css/main.css">
 
-  <?php include 'core/lib.header.php'; ?>
+  <?php 
+    include 'core/lib.header.php';
+    $list = []; 
+  ?>
 
   <script>
     $(document).ready(function () {
-       $('#exportListTable').on('click', 'button', function() {
+      var exportList = <?php echo json_encode($list); ?>;
+
+      $('#searchTable').on('click', 'button', function() {
+        var empID = $(this).attr('ref');
+        exportList.push(empID);
+        console.log(exportList);
+        $('#searchTable tr.'+ empID).remove();
+        var markup = "<tr class='"+empID+"'><td><small>"+empID+"<small></td><td class='text-right'><button type='button' ref='"+empID+"' class='btn btn-outline-danger btn-sm'>&times;</button></td></tr>";
+        $("#exportListTable tbody").append(markup);
+      });
+      
+      $('#exportListTable').on('click', 'button', function() {
         var delID = $(this).attr('ref');
-        // console.log(delID);
         $('#exportListTable tr.'+ delID).remove();
-       });
+        var markup = "<tr class='"+delID+"'><td><small>"+delID+"<small></td><td class='text-right'><button type='button' ref='"+delID+"' class='btn btn-outline-danger btn-sm'>&times;</button></td></tr>";
+        $("#searchTable tbody").append(markup);
+      });
+      
     });
   </script>
 </head>
@@ -63,28 +79,57 @@
             </thead>
             <tbody>
               <?php 
-              for($i=0; $i<5; $i++){ ?>
-              <tr>
+              $employees = array(
+                "hansak" => array(
+                  "id" => "hansakID123",
+                  "nameTH" => "หาญศักดิ์ อิสริยะทิพย์",
+                  "nameEN" => "Hansak Aisariyatip",
+                  "position" => "AVP - Portfolio Project Manager",
+                  "department" => "Information Technology",
+                  "mobile" => "+668 4024 5800",
+                  "email" => "hansak.a@assetworld.co.th"
+                ),
+                "john" => array(
+                  "id" => "johnID123",
+                  "nameTH" => "จอร์น",
+                  "nameEN" => "John Lenon",
+                  "position" => "Sale Manager",
+                  "department" => "Marketing",
+                  "mobile" => "+668 4024 5800",
+                  "email" => "john.l@assetworld.co.th"
+                ),
+                "peter" => array(
+                  "id" => "peterID123",
+                  "nameTH" => "ปีเตอร์",
+                  "nameEN" => "Peter Parker",
+                  "position" => "Project Co",
+                  "department" => "Information Technology",
+                  "mobile" => "+668 4024 5800",
+                  "email" => "peter.p@assetworld.co.th"
+                ),
+              );
+              foreach($employees as $e){ ?>
+              <tr class="<?php echo $e['id']; ?>">
                 <td>
-                  <small>หาญศักดิ์ อิสริยะทิพย์</small>
+                  <small><?php echo $e['nameTH']; ?></small>
                 </td>
                 <td>
-                  <small>Hansak Aisariyatip</small>
+                  <small><?php echo $e['nameEN']; ?></small>
                 </td>
                 <td>
-                  <small>AVP - Portfolio Project Manager</small>
+                  <small><?php echo $e['position']; ?></small>
                 </td>
                 <td>
-                  <small>Information Technology</small>
+                  <small><?php echo $e['department']; ?></small>
                 </td>
                 <td>
-                  <small>+668 4024 5800</small>
+                  <small><?php echo $e['mobile']; ?></small>
                 </td>
                 <td>
-                  <small>hansak.a@assetworld.co.th</small>
+                  <small><?php echo $e['email']; ?></small>
                 </td>
                 <td class="text-center">
-                  <button type="button" class="btn btn-outline-success btn-sm" id="addBtn">Add</button>
+                  <button type="button" class="btn btn-outline-success btn-sm addSearchBtn" ref="<?php echo $e['id']; ?>">Add</button>
                   <a href="user.php?empID=123" class="btn btn-outline-info btn-sm" role="button">View</a>
                 </td>
               </tr>
@@ -108,14 +153,11 @@
               <?php 
               $fakeList = ['Hansak','John','Peter'];
               foreach($fakeList as $flist){ ?>
-              <tr class="<?php echo $flist; ?>">
-                <td>
-                  <small><?php echo $flist; ?></small>
-                </td>
-                <td class="text-center">
-                  <button type="button" ref="<?php echo $flist; ?>" class="btn btn-outline-danger btn-sm">&times;</button>
-                </td>
-              </tr>
+                <tr class="<?php echo $flist; ?>">
+                  <td><small><?php echo $flist; ?><small></td>
+                  <td class='text-right'>
+                    <button type='button' ref="<?php echo $flist; ?>" class='btn btn-outline-danger btn-sm'>&times;</button>
+                </td></tr>
               <?php } ?>
             </tbody>
           </table>
